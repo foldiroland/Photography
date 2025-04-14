ec68ebb60823f24ce57d5f56f9677f0404738d02
foldyroland <foldyroland@gmail.com>
2025-04-14 19:43:50 +0200

    na azert
    asdadadadadsasd

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,11 @@
 function submit(){
     var password = document.getElementById("submit").value;
+    var username = document.getElementById("username").value;
 
     if (password == "admin123"){
         window.location.href = "../HTML/admin.html";
     }else{
         alert("Wrong password!");
     }
+
 }
